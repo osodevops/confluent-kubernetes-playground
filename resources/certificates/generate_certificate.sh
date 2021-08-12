12f02612b0145141204709aa93611f45cd3057bb
Sion Smith <sion.smith@gmail.com>
2021-08-12 15:22:15 +0100

    updated working replicator example after MANY tries!!

--- resources/certificates/generate_certificate.sh
+++ resources/certificates/generate_certificate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+SERVER_DOMAINS=${1-base-server-domain.json}
 cfssl gencert -initca base-ca-csr.json | cfssljson -bare ./sensitive-ca -
 # Verify with this:
 #openssl x509 -in sensitive-ca.pem -text -noout
-cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server base-server-domain.json | cfssljson -bare sensitive-server
+cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server $SERVER_DOMAINS | cfssljson -bare sensitive-server
 # Verify with this:
 #openssl x509 -in sensitive-server.pem -text -noout
 
@@ -11,13 +12,13 @@ kubectl create secret generic tls-group1 \
   --dry-run=client \
   --from-file=fullchain.pem=./sensitive-server.pem \
   --from-file=cacerts.pem=./sensitive-ca.pem \
-  --from-file=privkey.pem=./sensitive-server-key.pem -o yaml > ../../kustomize/base/secrets/tls-group1.yaml
+  --from-file=privkey.pem=./sensitive-server-key.pem -o yaml > ../../kustomize/base/secrets-tls/tls-group1.yaml
 
 # This is required for 'auto-generated' example
 kubectl create secret tls ca-pair-sslcerts \
 --dry-run=client \
 --cert=sensitive-ca.pem \
---key=sensitive-ca-key.pem -o yaml > ../../kustomize/base/secrets/ca-pair-sslcerts.yaml
+--key=sensitive-ca-key.pem -o yaml > ../../kustomize/base/secrets-tls/ca-pair-sslcerts.yaml
 
 md5sum sensitive-ca.pem
 md5sum sensitive-server.pem

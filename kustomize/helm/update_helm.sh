2dad862a7f578557ec469940fb44d23817d436bd
Andrew McCully <mccullya@gmail.com>
2021-08-20 14:53:35 +0100

    paramaterise update script

--- kustomize/helm/update_helm.sh
+++ kustomize/helm/update_helm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export CHART_VERSION=0.174.21
+#export CHART_VERSION=0.174.21
+export CHART_VERSION=0.174.13
 helm repo add confluentinc https://packages.confluent.io/helm
 helm search repo confluent --versions
 helm template confluentinc/confluent-for-kubernetes --version $CHART_VERSION --include-crds --output-dir .

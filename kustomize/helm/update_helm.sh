2ca1c77e154c436dd486f9686f8e9f712f018246
Andrew McCully <mccullya@gmail.com>
2021-08-20 14:40:14 +0100

    Adding helm update script

---
+++ kustomize/helm/update_helm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export CHART_VERSION=0.174.21
+helm repo add confluentinc https://packages.confluent.io/helm
+helm search repo confluent --versions
+helm template confluentinc/confluent-for-kubernetes --version $CHART_VERSION --include-crds --output-dir .
+mv confluent-for-kubernetes/crds/* ../crds/crds
+mv confluent-for-kubernetes/templates/* ../base/operator
+rm -R confluent-for-kubernetes

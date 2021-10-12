cdd2f2f6d22d397deaa17b45bde05d594807dfc8
Andrew McCully <mccullya@gmail.com>
2021-10-12 12:50:10 +0100

    fixing upgrade script for cluster roles

--- kustomize/helm/update_helm.sh
+++ kustomize/helm/update_helm.sh
@@ -4,7 +4,7 @@ export CHART_VERSION=0.174.25
 #export CHART_VERSION=0.174.13
 helm repo add confluentinc https://packages.confluent.io/helm
 helm search repo confluent --versions
-helm template confluentinc/confluent-for-kubernetes --version $CHART_VERSION --include-crds --output-dir .
+helm template confluentinc/confluent-for-kubernetes --version $CHART_VERSION --include-crds --set namespaced=false --output-dir .
 mv confluent-for-kubernetes/crds/* ../crds/crds
 mv confluent-for-kubernetes/templates/* ../base/operator
 rm -R confluent-for-kubernetes

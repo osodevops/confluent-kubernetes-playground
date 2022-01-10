b9fcb67f01ab5b974958c06919f1dff1ebcad584
Andrew McCully <mccullya@gmail.com>
2022-01-10 13:00:12 +0000

    WIP of mtls

--- incubator/userprovided-mtls/tls/generate_certificates.sh
+++ incubator/userprovided-mtls/tls/generate_certificates.sh
@@ -27,3 +27,16 @@ kubectl create secret generic tls-kafka \
   --from-file=fullchain.pem=./kafka-server.pem \
   --from-file=cacerts.pem=./root-ca.pem \
   --from-file=privkey.pem=./kafka-server-key.pem -o yaml > kafka-sslcerts.yaml
+
+# Create Client Certificates
+# Use the SANs listed in kafka-server-domain.json
+cfssl gencert -ca=root-ca.pem \
+-ca-key=root-ca-key.pem \
+-config=base-ca-config.json \
+-profile=server client-producer.json | cfssljson -bare kafka-client
+
+kubectl create secret generic tls-kafka \ connect to your Kafka cluster(s).
+  --dry-run=client \
+  --from-file=fullchain.pem=./kafka-client.pem \
+  --from-file=cacerts.pem=./root-ca.pem \
+  --from-file=privkey.pem=./kafka-client-key.pem -o yaml > kafka-client-sslcerts.yaml

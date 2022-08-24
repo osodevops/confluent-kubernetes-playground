e6ab26127836879ed795674bed8c8ab835141ba8
Andrew McCully <mccullya@gmail.com>
2022-08-24 10:31:09 +0100

    adjusted for dc1/dc2

--- incubator/disaster-recovery-replicator/external-certificates/generate_certificate.sh
+++ incubator/disaster-recovery-replicator/external-certificates/generate_certificate.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
+# This will generate the files:
+# * sensitive-ca.csr
+# * sensitive-ca.pem
+# * sensitive-ca-key.pem
 cfssl gencert -initca base-ca-csr.json | cfssljson -bare ./sensitive-ca -
-# Verify with this:
-#openssl x509 -in sensitive-ca.pem -text -noout
-cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server dc1-domain.json | cfssljson -bare sensitive-server
 
-cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server client-abc-domain.json | cfssljson -bare sensitive-client
 # Verify with this:
-#openssl x509 -in sensitive-server.pem -text -noout
+openssl x509 -in sensitive-ca.pem -text -noout
 
-# this is required for the 'custom-tls' example
-kubectl create secret generic abc-tls \
-  --dry-run=client \
-  --from-file=fullchain.pem=./sensitive-server.pem \
-  --from-file=cacerts.pem=./sensitive-ca.pem \
-  --from-file=privkey.pem=./sensitive-server-key.pem -o yaml > ./abc-tls.yaml
+
+# This will generate the files:
+# * sensitive-dc1.csr
+# * sensitive-dc1.pem
+# * sensitive-dc1-key.pem
+cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server dc1-domain.json | cfssljson -bare sensitive-dc1
+# Verify with this:
+openssl x509 -in sensitive-dc1.pem -text -noout
+
+
+# This will generate the files:
+# * sensitive-dc2.csr
+# * sensitive-dc2.pem
+# * sensitive-dc2-key.pem
+cfssl gencert -ca=./sensitive-ca.pem -ca-key=./sensitive-ca-key.pem -config=./base-ca-config.json -profile=server dc2-domain.json | cfssljson -bare sensitive-dc2
+# Verify with this:
+openssl x509 -in sensitive-dc2.pem -text -noout
+
+
+## CREATE KUBERNETES SECRETS
+# DC1 Certificates
+kubectl create secret generic dc1-tls --dry-run=client \
+--from-file=fullchain.pem=./sensitive-dc1.pem \
+--from-file=privkey.pem=./sensitive-dc1-key.pem \
+--from-file=cacerts.pem=./sensitive-ca.pem -o yaml > ./dc1-tls.yaml
+# DC2 Certificates
+kubectl create secret generic dc2-tls --dry-run=client \
+--from-file=fullchain.pem=./sensitive-dc2.pem \
+--from-file=privkey.pem=./sensitive-dc2-key.pem \
+--from-file=cacerts.pem=./sensitive-ca.pem -o yaml > ./dc2-tls.yaml
 
 # This is required for 'auto-generated' example
 kubectl create secret tls ca-pair-sslcerts \
 --dry-run=client \
 --cert=sensitive-ca.pem \
---key=sensitive-ca-key.pem -o yaml > ./abc-ca-pair-sslcerts.yaml
-
-md5sum sensitive-ca.pem
-md5sum sensitive-server.pem
+--key=sensitive-ca-key.pem -o yaml > ./generic-ca-pair-sslcerts.yaml

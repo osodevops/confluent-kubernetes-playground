c54e8f743ccf5772f3df74dc7dbe0d9dd8f307f2
Andrew I McCully <mccullya@gmail.com>
2022-11-17 09:24:35 +0000

    adding 2.5 to base

--- scripts/update_helm.sh
+++ scripts/update_helm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-export APP_VERSION=2.4.1
-export CHART_VERSION=0.517.23
+export APP_VERSION=2.5.0
+export CHART_VERSION=0.581.16
+#export APP_VERSION=2.4.1
+#export CHART_VERSION=0.517.23
 #export APP_VERSION=2.4.0
 #export CHART_VERSION=0.517.12
 #export APP_VERSION=2.3.0
